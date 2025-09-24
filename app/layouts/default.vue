ac3a5ce45edc940eebb2fccfc63484c34ef0e260
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2025-09-24 02:10:17 -0500

    feat: version 0001

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -42,10 +42,13 @@ const items = [
             <SidebarMenu>
                 <SidebarMenuItem v-for="item in items" :key="item.title">
                   <SidebarMenuButton asChild>
-                      <a :href="item.url" :tooltip="item.title">
-                        <component :is="item.icon" />
-                        <span>{{item.title}}</span>
-                      </a>
+                    <NuxtLink
+                      class="flex items-center gap-2"
+                      :to="item.url"
+                      :tooltip="item.title">
+                      <component :is="item.icon" />
+                      <span>{{item.title}}</span>
+                    </NuxtLink>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
             </SidebarMenu>
